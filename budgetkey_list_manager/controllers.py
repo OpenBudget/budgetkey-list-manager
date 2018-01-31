4ee6de085a8b41bf1cc9c3c9ce9a1e48fcf00513
Ori Hoch <ori@uumpa.com>
2018-01-31 15:49:51 +0200

    basic list methods, e2e test, default port 5050 to prevent collision with auth server

--- budgetkey_list_manager/controllers.py
+++ budgetkey_list_manager/controllers.py
@@ -1,4 +1,32 @@
-def store(permissions, list, item):
-    # Check that permissions user matches list owner
-    # append item to list (if doesn't exist already
+from .models import get_list, add_item, create_list, get_items, delete_item, get_list_by_item
+
+
+def store(permissions, list_name, item):
+    user_id = permissions.get("profile", {}).get("id")
+    if not user_id:
+        return False
+    if not get_list(list_name, user_id):
+        create_list(list_name, user_id)
+    add_item(list_name, user_id, item)
+    return True
+
+
+def get(permissions, list_name):
+    user_id = permissions.get("profile", {}).get("id")
+    if not user_id:
+        return False
+    list = get_list(list_name, user_id)
+    if not list:
+        return False
+    return {"id": list["id"], "items": get_items(list_name, user_id)}
+
+
+def delete(permissions, item_id):
+    user_id = permissions.get("profile", {}).get("id")
+    if not user_id:
+        return False
+    list = get_list_by_item(item_id)
+    if not list or list["user_id"] != user_id:
+        return False
+    delete_item(item_id)
     return True
